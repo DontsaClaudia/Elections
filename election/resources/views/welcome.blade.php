966060844ebc1991072bbaf1c21d0b7688d44661
DontsaClaudia <donstsaclaudia@gmail.com>
2023-04-17 12:28:30 +0200

    ajout des dossiers du projet de classe php et angular

---
+++ election/resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @vite(['resources/js/app.js'])
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+       
+
+        <!-- Styles -->
+       
+    </head>
+    <body>
+        <h1 class="alert alert-success"> bonjour csi3 </h1>
+        <a href="/regions_create"> create region </a>
+    </body>
+</html>
